e6481f827df06644e82c0d861e61e74eea14506b
Piyush Rungta <piyushrungta25@gmail.com>
2019-12-07 20:28:46 +0530

    Optimize day6 solution for speed
    Reduced the runtime by about 40% on my very unscientific benchmark.
    - Algorithmic improvements in the function to find the first common
      element in two arrays
    - Minimized some memory allocations

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,35 +2,22 @@ use std::collections::{HashMap, VecDeque};
 use std::error::Error;
 use std::fs;
 
-fn parse_input() -> Result<HashMap<String, Vec<String>>, Box<dyn Error>> {
-    let buf: String = fs::read_to_string("input")?;
-    let mut orbits: HashMap<String, Vec<String>> = HashMap::new();
+fn parse_input<'a>(input: &'a str) -> Result<(HashMap<&'a str, Vec<&'a str>>, HashMap<&'a str, &'a str>) , Box<dyn Error>> {
+    let mut orbits: HashMap<&str, Vec<&str>> = HashMap::with_capacity(2000);
+    let mut orbits_rev: HashMap<&str, &str> = HashMap::with_capacity(2000);
 
-    buf.trim().split("\n").for_each(|line| {
-        let mut objs = line.trim().split(")").map(str::to_string);
+    input.trim().split("\n").for_each(|line| {
+        let mut objs = line.trim().split(")");
         let parent = objs.next().unwrap();
         let child = objs.next().unwrap();
         orbits.entry(parent).or_default().push(child);
+        orbits_rev.entry(child).or_insert(parent);
     });
 
-    Ok(orbits)
+    Ok((orbits, orbits_rev))
 }
 
-fn parse_input_reverse() -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let buf: String = fs::read_to_string("input")?;
-    let mut orbits: HashMap<String, String> = HashMap::new();
-
-    buf.trim().split("\n").for_each(|line| {
-        let mut objs = line.trim().split(")").map(str::to_string);
-        let parent = objs.next().unwrap();
-        let child = objs.next().unwrap();
-        orbits.entry(child).or_insert(parent);
-    });
-
-    Ok(orbits)
-}
-
-fn orbit_count_checksum(m: HashMap<String, Vec<String>>) -> u32 {
+fn orbit_count_checksum(m: HashMap<&str, Vec<&str>>) -> u32 {
     let mut queue = VecDeque::new();
     queue.push_back(("COM", 1));
     let mut count: u32 = 0;
@@ -47,41 +34,43 @@ fn orbit_count_checksum(m: HashMap<String, Vec<String>>) -> u32 {
     count
 }
 
-fn parents<'a>(m: &'a HashMap<String, String>, from: &'static str) -> Vec<&'a str> {
+fn parents<'a>(m: &'a HashMap<&'a str, &'a str>, from: &'static str) -> Vec<&'a str> {
     let mut cur = from;
     let mut parents = Vec::new();
     while cur != "COM" {
         if let Some(parent) = m.get(cur) {
-            parents.push(parent.as_str());
+            parents.push(*parent);
             cur = parent;
         }
     }
     parents
 }
 
-fn first_common_element(from: &Vec<&str>, to: &Vec<&str>) -> Option<(usize, usize)> {
+fn first_common_element(from: Vec<&str>, to: Vec<&str>) -> Option<(usize, usize)> {
+    let mut hm: HashMap<&str, usize> = HashMap::new();
+    for (i, item) in to.into_iter().enumerate() {
+        hm.insert(item, i);
+    }
     for (i, f) in from.iter().enumerate() {
-        for (j, t) in to.iter().enumerate() {
-            if f == t {
-                return Some((i, j));
-            }
+        if let Some(j) = hm.get(f) {
+            return Some((i, *j));
+
         }
     }
     None
 }
 
-fn min_orbital_jumps(m: HashMap<String, String>, from: &'static str, to: &'static str) -> usize {
+fn min_orbital_jumps(m: HashMap<&str, &str>, from: &'static str, to: &'static str) -> usize {
     let from_parents = parents(&m, from);
     let to_parents = parents(&m, to);
 
-    let (i, j) = first_common_element(&from_parents, &to_parents).unwrap();
+    let (i, j) = first_common_element(from_parents, to_parents).unwrap();
     i + j
 }
 
 fn main() {
-    let m = parse_input().unwrap();
-    println!("Part 1: {:?}", orbit_count_checksum(m));
-
-    let m = parse_input_reverse().unwrap();
-    println!("Part 2: {:?}", min_orbital_jumps(m, "YOU", "SAN"));
+    let inp = fs::read_to_string("input").unwrap();
+    let (m1, m2) = parse_input(&inp).unwrap();
+    println!("Part 1: {:?}", orbit_count_checksum(m1));
+    println!("Part 2: {:?}", min_orbital_jumps(m2, "YOU", "SAN"));
 }
