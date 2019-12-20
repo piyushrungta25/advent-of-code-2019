c6bc2478437b2cc43aae9940d66edb560803ab1b
Piyush Rungta <piyushrungta25@gmail.com>
2019-12-20 17:43:58 +0530

    Remove some dead code
    Also add some comments as to what is going on

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,6 +1,5 @@
 use std::error::Error;
 use std::fs;
-use std::cmp::{min, max};
 use std::f64::consts::PI;
 
 
@@ -12,49 +11,31 @@ fn get_input(file: &str) -> Result<Vec<Vec<i32>>, Box<dyn Error>> {
 }
 
 
-fn is_reachable(inp: &Vec<Vec<i32>>, x1: usize, y1: usize, x2: usize, y2: usize) -> bool {
-	let (x1, y1, x2, y2) = (x1 as i64, y1 as i64, x2 as i64, y2 as i64);
-	let is_on_line = |x, y| (y-y1)*(x2-x1) == (y2-y1) * (x-x1);
-
-	for x in min(x1, x2)..=max(x1, x2) {
-		for y in min(y1, y2)..=max(y1, y2) {
-			if (x==x1 && y==y1) || (x==x2 && y==y2) || inp[x as usize][y as usize] == 0 {
-				continue;
-			}
-
-			if is_on_line(x, y) {
-				return false;
-			}
-		}
-	}
-	true
+fn angle_from_y_axis((x, y): (i32, i32)) -> f64 {
+	// we invert the y-axis here
+	let mut q = (x as f64).atan2((y*-1) as f64);
+    if q < 0.0 {
+        q += 2.0*PI;
+    }
+    q
 }
 
-fn get_all_reachable(inp: &&Vec<Vec<i32>>, x1: usize, y1: usize) -> usize {
-	let mut count  = 0;
-
-	for (y, row) in inp.iter().enumerate() {
-		for (x, v) in row.iter().enumerate() {
-			if *v == 0 || (x==x1 && y==y1) {
-				continue;
-			}
-			if is_reachable(&inp, x1, y1, x, y) {
-				count+=1;
-			}
-		}
-	}
-
-	count
+fn distance_from_origin((x, y): (i32, i32)) -> f64 {
+	((x*x + y*y) as f64).sqrt()
 }
 
-
+// calculates the angle every point makes with y-axis when origin is shifted to (x0, y0)
+// bins the point on same length and sort the points in each bin by their distance from (x0, y0)
 fn get_binned(inp: &Vec<Vec<i32>>,  (x0, y0): (usize, usize)) -> Vec<Vec<((i32, i32), f64, f64)>> {
 	let mut mapped = vec![];
+
 	for (y, row) in inp.iter().enumerate() {
 		for (x, v) in row.iter().enumerate() {
 			if *v != 1 || (x==x0 && y == y0) {
 				continue;
 			}
+
+			// shift the origin to (x0, y0)
 			let (x, y) = ((x as i32 - x0 as i32), (y as i32 - y0 as i32));
 			mapped.push(
 				((x, y), angle_from_y_axis((x, y)), distance_from_origin((x, y)))
@@ -62,6 +43,7 @@ fn get_binned(inp: &Vec<Vec<i32>>,  (x0, y0): (usize, usize)) -> Vec<Vec<((i32, 
 		}
 	}
 
+	// sort by the angle the make with the y-axis
 	mapped.sort_by(|x, y| (x.1).partial_cmp(&y.1).unwrap());
 
 	let mut i = 0;
@@ -74,6 +56,8 @@ fn get_binned(inp: &Vec<Vec<i32>>,  (x0, y0): (usize, usize)) -> Vec<Vec<((i32, 
 			bin.push(mapped[i]);
 			i+=1;
 		}
+
+		// sort the bin by distance from origin
 		bin.sort_by(|x, y| (x.2).partial_cmp(&y.2).unwrap());
 		binned.push(bin);
 	}
@@ -101,18 +85,6 @@ fn part1(inp: &Vec<Vec<i32>>) -> (usize, Option<(usize, usize)>) {
 	(max_count, cords)
 }
 
-fn angle_from_y_axis((x, y): (i32, i32)) -> f64 {
-	let mut q = (x as f64).atan2((y*-1) as f64);
-    if q < 0.0 {
-        q += 2.0*PI;
-    }
-    q
-}
-
-fn distance_from_origin((x, y): (i32, i32)) -> f64 {
-	((x*x + y*y) as f64).sqrt()
-}
-
 
 fn part2(inp: &Vec<Vec<i32>>, (x0, y0): (usize, usize)) -> (i32, i32) {
 	let mut binned = get_binned(inp, (x0, y0));
@@ -120,6 +92,9 @@ fn part2(inp: &Vec<Vec<i32>>, (x0, y0): (usize, usize)) -> (i32, i32) {
 	let mut count = 1;
 	let mut last_coord = (-1, -1);
 
+
+	// for the sweeping motion, we continiously loop over the bins and take the
+	// first value from each bin
 	'main_loop: while count <= 200 {
 		for bin in binned.iter_mut() {
 			if bin.len() > 0 {
@@ -149,22 +124,4 @@ fn main() {
 
    println!("Part 1: {}", part1);
    println!("Part 2: {}", part2);
-}
-
-#[cfg(test)]
-mod test {
-	use super::*;
-
-	#[test]
-	fn test_one() {
-		let (x1, y1, x2, y2) = (3, 4, 1, 0);
-		let is_on_line = |x, y| (y-y1)*(x2-x1) == (y2-y1) * (x-x1);
-		assert!(is_on_line(2,2));
-	}
-
-	#[test]
-	fn two() {
-	   let input = get_input("input1").unwrap();
-	   assert!(!is_reachable(&input, 3, 4, 1, 0));
-	}
 }
